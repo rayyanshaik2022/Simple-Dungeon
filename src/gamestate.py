0904f3c1d934c8d63d5e7145a1119e726e52ad4d
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2020-08-13 07:40:33 -0400

    Better restriction of movement

--- src/gamestate.py
+++ src/gamestate.py
@@ -2,6 +2,7 @@ import pygame
 import numpy as np
 import random
 import math
+from settings import *
 
 class GameState:
 
@@ -49,7 +50,7 @@ class GameState:
                 if self.lobby['selected_characters'][char] != None:
                     c += 1
             
-            if c >= 2: # Number of people required to start game
+            if c >= 1: # Number of people required to start game
                 self.lobby['start_countdown'] -= (1/60)
             else:
                 self.lobby['start_countdown'] = 5 # Reset timer (65)
@@ -69,22 +70,50 @@ class GameState:
                         'Spirit-Boxer' : {
                             'id_' : self.lobby['selected_characters']['Spirit-Boxer'],
                             'angle' : 0,
-                            'pos' : [3*128, 10*128],
-                            'speed' : 2
+                            'pos' : [5*TILE_SIZE, 12*TILE_SIZE],
+                            'speed' : 2,
+                            'size' : [16*PLAYER_SCALING, 19*PLAYER_SCALING]
                         },
                         'Samurai-Merchant' : {
                             'id_' : self.lobby['selected_characters']['Samurai-Merchant'],
                             'angle' : 0,
-                            'pos' : [3*128, 10*128],
-                            'speed' : 2
+                            'pos' : [5*TILE_SIZE, 12*TILE_SIZE],
+                            'speed' : 2,
+                            'size' : [38*PLAYER_SCALING, 24*PLAYER_SCALING]
                         }
                     }
                 }
 
             for char in self.game_information['players']:
                 character = self.game_information['players'][char]
-                character['pos'][0] += math.cos(character['angle']) * character['speed']
-                character['pos'][1] += math.sin(character['angle']) * character['speed']
+                # Move character
+                f_x = character['pos'][0] + math.cos(character['angle']) * character['speed']
+                f_y = character['pos'][1] + math.sin(character['angle']) * character['speed']
+
+                # Check if character is out of bounds
+                feet_pos = [int(f_x/TILE_SIZE), int((f_y + character['size'][1]/2)/TILE_SIZE)]
+                old_feet_pos = [int(character['pos'][0]/TILE_SIZE), int((character['pos'][1] + character['size'][1]/2)/TILE_SIZE)]
+                if (feet_pos[0] >= 0 and feet_pos[0] < len(self.map[0][0])) and (feet_pos[1] >= 0 and feet_pos[1] < len(self.map[0])):
+                    
+                    block_tiles = [0,3,1,4,10,5,8,9]
+                    for i in range(100,400,100):
+                        for item in block_tiles:
+                            if item not in block_tiles:
+                                block_tiles.append(item*i)
+
+                    if self.map[0][feet_pos[1]][feet_pos[0]] in block_tiles:
+
+                        if self.map[0][old_feet_pos[1]][feet_pos[0]] not in block_tiles:
+                            character['pos'] = [f_x, character['pos'][1]]
+                        elif self.map[0][feet_pos[1]][old_feet_pos[0]] not in block_tiles:
+                            character['pos'] = [character['pos'][0], f_y]
+                        else:
+                            # TODO: FIX CORNER UPDATES
+                            character['pos'] = [old_feet_pos[0]*TILE_SIZE, old_feet_pos[1]*TILE_SIZE]
+                    else:
+                        character['pos'] = [f_x, f_y]
+                else:
+                    character['pos'] = [f_x, f_y]
         
         if self.lobby['players_connected'] == {} and self.c_state not in [-1,0]:
             self.c_state = -1
